ba3c9bc3572bc42a66c2c7ab35f63823ce928d56
Murad Faridi <muradfaridi92@gmail.com>
2019-04-14 12:43:57 +0500

    Install node_module
    Node module is installed through npm install command

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    {{$title}}
+    <h1>{{$title}}</h1>
 @endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-	{{$title}}
+	<h1>{{$title}}</h1>
 	@if(count($services) > 0)
 		<ul>
 			@foreach ($services as $service)

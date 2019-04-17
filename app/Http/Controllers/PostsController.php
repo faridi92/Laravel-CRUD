e46c188cdbbeb3fd59e4bb01e446d3d51441c1bf
Murad Faridi <muradfaridi92@gmail.com>
2019-04-17 16:58:02 +0500

    Save Data in DB and Send Success Message

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -47,6 +47,13 @@ class PostsController extends Controller
             'body'  =>  'required'
         ]);
         
+        // Create Post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Created Successfully.');
     }
 
     /**

c1cd8fe6f82d2ee90b0f9837beb5da88f4152e52
Murad Faridi <muradfaridi92@gmail.com>
2019-04-17 17:12:09 +0500

    Add create post link in navbar

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -18,6 +18,11 @@
         <a class="nav-link" href="/posts">Blog</a>
       </li>
     </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/posts/create">Create Post</a>
+      </li>
+    </ul>
     <form class="form-inline mt-2 mt-md-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>

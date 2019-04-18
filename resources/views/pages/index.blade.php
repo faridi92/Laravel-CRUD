34ce4b5cb324d9e2288b020ed93d9cd17a1204b4
Murad Faridi <muradfaridi92@gmail.com>
2019-04-18 17:01:18 +0500

    check for user logged in

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,10 @@
     <div class="jumbotron text-center">
         <h1>{{$title}}</h1>
         <p>This is the Laravel Application by Murad Faridi.</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+        @if (Auth::guest())
+            <p>
+                <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+            </p>
+        @endif
     </div>
 @endsection

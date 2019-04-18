f7a842a5de8397bb036eadfa60beda0e5b08fde7
Murad Faridi <muradfaridi92@gmail.com>
2019-04-18 15:11:45 +0500

    Model relationship and user access

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,28 @@
                     <a class="btn btn-primary" href="/posts/create">Create Post</a>
                     <hr>
                     <h3>Your Blog Posts</h3>
+                    @if (count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{$post->title}}</td>
+                                    <td class="float-right">
+                                        {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'DELETE', 'class' => 'float-right']) !!}
+                                            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                        {!! Form::close() !!}
+                                    </td>
+                                    <td class="float-right"><a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a></td>
+                                </tr>
+                            @endforeach
+                        </table>  
+                    @else
+                        <p>You have no posts.</p>
+                    @endif
                 </div>
             </div>
         </div>

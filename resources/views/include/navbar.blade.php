921e70b2a5a355d704bbc381177bdf1759e48a5b
Murad Faridi <muradfaridi92@gmail.com>
2019-04-18 10:40:02 +0500

    Get logged in user_id from session
    Store logged in user_id in Posts

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -10,9 +10,6 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/about">About</a>
                 </li>
@@ -43,6 +40,7 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('home') }}">Home </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
